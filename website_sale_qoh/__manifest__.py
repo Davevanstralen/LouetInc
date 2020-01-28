e470046a2c7cdb0692b250f167bfbc53e0746601
Prashant Panchal <ppa@odoo.com>
2020-01-28 16:39:36 +0530

    [ADD] init the moduel for the Qty On Hand - Outgoing Quantity

---
+++ website_sale_qoh/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'eCommerce Qty On Hand',
+    'summary': 'eCommerce Qty On Hand',
+    'sequence': 100,
+    'license': 'OEEL-1',
+    'website': 'https://www.odoo.com',
+    'version': '1.0',
+    'author': 'Odoo Inc',
+    'description': """
+eCommerce Qty On Hand
+=====================
+    """,
+    'category': 'Custom Development',
+    'depends': ['sale', 'website_sale', 'website_sale_stock'],
+    'data': [
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
