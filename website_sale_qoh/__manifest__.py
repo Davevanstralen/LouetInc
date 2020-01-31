e66f05244f274898e6eabe4c0f00ed4f3d826a1c
Prashant Panchal <ppa@odoo.com>
2020-01-31 10:56:43 +0530

    [ADD]  allow the customer to place an order even if the product is not in stock. Incoming Quantity should not be taken into consideration until its in Inventory.

--- website_sale_qoh/__manifest__.py
+++ website_sale_qoh/__manifest__.py
@@ -15,9 +15,10 @@ eCommerce Qty On Hand
     'category': 'Custom Development',
     'depends': ['sale', 'website_sale', 'website_sale_stock'],
     'data': [
+        'views/website_sale_stock_template.xml',
     ],
     'demo': [],
-    'qweb': [],
+    'qweb': ['static/src/xml/website_sale_stock_product_availability.xml'],
     'installable': True,
     'application': False,
     'auto_install': False,
