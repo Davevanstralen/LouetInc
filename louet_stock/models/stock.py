762f766dc2ae880e0c4a4fc259a57ec0b9b849e7
cic-odoo <cic@odoo.com>
2020-03-26 17:17:21 -0700

    [IMP] sale order line pass through

--- louet_stock/models/stock.py
+++ louet_stock/models/stock.py
@@ -8,6 +8,13 @@ from odoo import api, fields, models, _
 from datetime import datetime, timedelta
 
 
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def _prepare_procurement_values(self):
+        return super(StockMove, self)._prepare_procurement_values().update({'sale_line_id': self.sale_line_id.id})
+
+
 class PickingType(models.Model):
     _inherit = 'stock.picking.type'
 
@@ -20,39 +27,54 @@ class Picking(models.Model):
 
     def create_csv_data(self):
         sale_id = self.group_id.sale_id
-        lines = [{'number': self.name,
-                  'date': sale_id.date_order,
-                  'sales_order_number': sale_id.name,
-                  'currency': sale_id.currency_id,
-                  'soldto_company': '',
-                  'soldto_address': '',
-                  'soldto_address2': '',
-                  'soldto_city': '',
-                  'soldto_state_province': '',
-                  'soldto_postal_code': '',
-                  'soldto_country': '',
-                  'soldto_telephone': '',
-                  'soldto_contact_email': '',
-                  'soldto_tax_id': '',
-                  'shipto_company': '',
-                  'shipto_address1': '',
-                  'shipto_address2': '',
-                  'shipto_city': '',
-                  'shipto_state_provice': '',
-                  'shipto_postal_code': '',
-                  'shipto_country': '',
-                  'shipto_contact_telephone': '',
-                  'shipto_contact_email': '',
-                  'shipto_tax_id': '',
-                  'ship_by_date': '',
-                  'deliver_by_date': '',
-                  'line_number': '',
-                  'part_number': '',
-                  'part_description': '',
-                  'quantity_ordered': '',
-                  'quantity_shiped': '',
-                  'unit_price': '',
-                  'database_type': ''}]
+
+        rows = []
+
+        for line in self.move_line_ids_without_package:
+            test_price = line.move_id.sale_line_id
+            rows.append({'number': self.name or '',
+                         # 'date': sale_id.date_order,
+                         # 'sales_order_number': sale_id.name,
+                         'currency': sale_id.currency_id.name or '',
+                         'soldto_company': sale_id.partner_id.name or '',
+                         'soldto_address': sale_id.partner_id.street or '',
+                         # 'soldto_address2': '',
+                         'soldto_city': sale_id.partner_id.city or '',
+                         'soldto_state_province': sale_id.partner_id.state_id.name or '',
+                         'soldto_postal_code': sale_id.partner_id.zip or '',
+                         'soldto_country': sale_id.partner_id.country_id.name or '',
+                         # 'soldto_contact_name': '',
+                         # 'soldto_telephone': '',
+                         # 'soldto_contact_email': '',
+                         'soldto_tax_id': sale_id.partner_id.vat or '',
+                         'shipto_company': sale_id.partner_invoice_id.name or '',
+                         'shipto_address1': sale_id.partner_invoice_id.street or '',
+                         # 'shipto_address2': '',
+                         'shipto_city': sale_id.partner_invoice_id.city or '',
+                         'shipto_state_provice': sale_id.partner_invoice_id.state_id.name or '',
+                         'shipto_postal_code': sale_id.partner_invoice_id.zip or '',
+                         'shipto_country': sale_id.partner_invoice_id.country_id.name or '',
+                         # 'soldto_contact_name': '',
+                         # 'shipto_contact_telephone': '',
+                         # 'shipto_contact_email': '',
+                         'shipto_tax_id': sale_id.partner_invoice_id.vat or '',
+                         'bill_freight_to_company': sale_id.partner_shipping_id.name or '',
+                         'bill_freight_to_address1': sale_id.partner_shipping_id.street or '',
+                         # 'bill_freight_to_address2': '',
+                         'bill_freight_to_city': sale_id.partner_shipping_id.city or '',
+                         'bill_freight_to_state_province': sale_id.partner_shipping_id.state_id.name or '',
+                         'bill_freight_to_postal_code': sale_id.partner_shipping_id.zip or '',
+                         'bill_freight_to_country': sale_id.partner_shipping_id.country_id.name or '',
+                         # 'ship_by_date': '',
+                         'deliver_by_date': sale_id.commitment_date or '',
+                         'line_number': line.id or '',
+                         'part_number': line.product_id.name or '',
+                         'part_description': line.product_id.default_code or '',
+                         'quantity_ordered': line.qty_done,
+                         # 'quantity_shiped': '',
+                         'unit_price': line.move_id.sale_line_id.price_unit,
+                         'database_type': 'Test'})
+        return rows
 
     # Create xls file for broker report
     def create_broker_report(self):
@@ -62,19 +84,18 @@ class Picking(models.Model):
         print(filename)
 
         with open(filename, mode='w') as broker_file:
-            fieldnames = ['number', 'date', 'sales_order_number', 'currency', 'soldto_company', 'soldto_address',
-                          'soldto_address2', 'soldto_city', 'soldto_state_province', 'soldto_postal_code',
-                          'soldto_country',
-                          'soldto_telephone', 'soldto_contact_email', 'soldto_tax_id', 'shipto_company',
-                          'shipto_address1',
-                          'shipto_address2', 'shipto_city', 'shipto_state_provice', 'shipto_postal_code',
-                          'shipto_country',
-                          'shipto_contact_telephone', 'shipto_contact_email', 'shipto_tax_id', 'ship_by_date',
-                          'deliver_by_date', 'line_number', 'part_number', 'part_description', 'quantity_ordered',
-                          'quantity_shiped', 'unit_price', 'database_type']
+            # fieldnames = ['number', 'date', 'sales_order_number', 'currency', 'soldto_company', 'soldto_address', 'soldto_address2', 'soldto_city', 'soldto_state_province', 'soldto_postal_code', 'soldto_country', 'soldto_telephone', 'soldto_contact_email', 'soldto_tax_id', 'shipto_company', 'shipto_address1', 'shipto_address2', 'shipto_city', 'shipto_state_provice', 'shipto_postal_code', 'shipto_country', 'shipto_contact_telephone', 'shipto_contact_email', 'shipto_tax_id', 'ship_by_date', 'deliver_by_date', 'line_number', 'part_number', 'part_description', 'quantity_ordered', 'quantity_shiped', 'unit_price', 'database_type']
+            fieldnames = ['number', 'currency', 'soldto_company', 'soldto_address', 'soldto_city',
+                          'soldto_state_province', 'soldto_postal_code', 'soldto_country', 'soldto_tax_id',
+                          'shipto_company', 'shipto_address1', 'shipto_city', 'shipto_state_provice',
+                          'shipto_postal_code', 'shipto_country', 'shipto_tax_id', 'bill_freight_to_company',
+                          'bill_freight_to_address1', 'bill_freight_to_city', 'bill_freight_to_state_province',
+                          'bill_freight_to_postal_code', 'bill_freight_to_country', 'deliver_by_date', 'line_number',
+                          'part_number', 'part_description', 'quantity_ordered', 'unit_price', 'database_type']
             print(len(fieldnames))
             writer = csv.DictWriter(broker_file, fieldnames=fieldnames)
             writer.writeheader()
+            writer.writerows(self.create_csv_data())
 
         with open(filename, mode='rb') as broker_file:
             file_base64 = base64.b64encode(broker_file.read())
