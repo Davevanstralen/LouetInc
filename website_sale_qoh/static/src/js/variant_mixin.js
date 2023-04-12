4745e9a39f74fd4a3b0b6a69956d6c3355ba3f74
yuro-odoo <yuro@odoo.com>
2023-04-12 12:24:13 -0700

    [FIX]fixed website frontend error

--- website_sale_qoh/static/src/js/variant_mixin.js
+++ website_sale_qoh/static/src/js/variant_mixin.js
@@ -1,68 +1,62 @@
-/** @odoo-module **/
-
-import VariantMixin from "website_sale_stock.VariantMixin";
-import publicWidget from "web.public.widget";
-import core from "web.core";
-import { Markup } from "web.utils";
-import WebsiteSale from "website_sale.website_sale";
-const QWeb = core.qweb;
-
-function updateQuantityLouet(widget, ev, $parent, combination) {
-    let product_id = 0;
-    if ($parent.find('input.product_id:checked').length) {
-        product_id = $parent.find('input.product_id:checked').val();
-    } else {
-        product_id = $parent.find('.product_id').val();
-    }
-    const isMainProduct = combination.product_id &&
-        ($parent.is('.js_main_product') || $parent.is('.main_product')) &&
-        combination.product_id === parseInt(product_id);
-
-    if (!widget.isWebsite || !isMainProduct) {
-        return;
-    }
-
-    const $addQtyInput = $parent.find('input[name="add_qty"]');
-    let qty = $addQtyInput.val();
-    let ctaWrapper = $parent[0].querySelector('#o_wsale_cta_wrapper');
-    ctaWrapper.classList.replace('d-none', 'd-flex');
-    ctaWrapper.classList.remove('out_of_stock');
-
-    if (combination.product_type === 'product' && !combination.allow_out_of_stock_order) {
-        combination.free_qty -= parseInt(combination.cart_qty);
-        $addQtyInput.data('max', combination.free_qty || 1);
-        if (combination.free_qty < 0) {
-            combination.free_qty = 0;
+odoo.define('website_sale_qoh.VariantMixin', function (require) {
+    'use strict';
+    
+    const {Markup} = require('web.utils');
+    var website_sale_stock_VariantMixin = require('website_sale_stock.VariantMixin')
+    var core = require('web.core');
+    var QWeb = core.qweb;
+    
+    website_sale_stock_VariantMixin._onChangeCombinationStock = function (ev, $parent, combination) {
+        let product_id = 0;
+        if ($parent.find('input.product_id:checked').length) {
+            product_id = $parent.find('input.product_id:checked').val();
+        } else {
+            product_id = $parent.find('.product_id').val();
         }
-        if (qty > combination.free_qty) {
-            qty = combination.free_qty || 1;
-            $addQtyInput.val(qty);
+        const isMainProduct = combination.product_id &&
+            ($parent.is('.js_main_product') || $parent.is('.main_product')) &&
+            combination.product_id === parseInt(product_id);
+    
+        if (!this.isWebsite || !isMainProduct){
+            return;
         }
-        if (combination.free_qty < 1) {
-            ctaWrapper.classList.replace('d-flex', 'd-none');
-            ctaWrapper.classList.add('out_of_stock');
+    
+        const $addQtyInput = $parent.find('input[name="add_qty"]');
+        let qty = $addQtyInput.val();
+        let ctaWrapper = $parent[0].querySelector('#o_wsale_cta_wrapper');
+        ctaWrapper.classList.replace('d-none', 'd-flex');
+        ctaWrapper.classList.remove('out_of_stock');
+    
+        if (combination.product_type === 'product' && !combination.allow_out_of_stock_order) {
+            combination.free_qty -= parseInt(combination.cart_qty);
+            $addQtyInput.data('max', combination.free_qty || 1);
+            if (combination.free_qty < 0) {
+                combination.free_qty = 0;
+            }
+            if (qty > combination.free_qty) {
+                qty = combination.free_qty || 1;
+                $addQtyInput.val(qty);
+            }
+            if (combination.free_qty < 1) {
+                ctaWrapper.classList.replace('d-flex', 'd-none');
+                ctaWrapper.classList.add('out_of_stock');
+            }
         }
-    }
-
-    if (combination.product_type === 'product' && combination.allow_out_of_stock_order) {
-        combination.free_qty -= parseInt(combination.cart_qty);
-    }
-
-    $('.oe_website_sale')
-        .find('.availability_message_' + combination.product_template)
-        .remove();
-    combination.has_out_of_stock_message = $(combination.out_of_stock_message).text() !== '';
-    combination.out_of_stock_message = Markup(combination.out_of_stock_message);
-    const $message = $(QWeb.render(
-        'website_sale_stock.product_availability',
-        combination
-    ));
-    $('div.availability_messages').html($message);
-};
-
-publicWidget.registry.WebsiteSale.include({
-    _onChangeCombination(){
-        this._super.apply(this, arguments);
-        updateQuantityLouet(this, ...arguments);
-    }
+    
+        if (combination.product_type === 'product' && combination.allow_out_of_stock_order) {
+            combination.free_qty -= parseInt(combination.cart_qty);
+        }
+    
+        $('.oe_website_sale')
+            .find('.availability_message_' + combination.product_template)
+            .remove();
+        combination.has_out_of_stock_message = $(combination.out_of_stock_message).text() !== '';
+        combination.out_of_stock_message = Markup(combination.out_of_stock_message);
+        const $message = $(QWeb.render(
+            'website_sale_stock.product_availability',
+            combination
+        ));
+        $('div.availability_messages').html($message);
+    };
 });
+    
